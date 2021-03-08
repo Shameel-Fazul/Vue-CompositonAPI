ba631490934e6bcf1f98b271cd92f98ba185f890
Shameel Fazul <shameel.fazul@mail.com>
2021-03-08 14:44:44 +0530

    Vue Composition API
    - Computed Values
    - Watch & WatchEffect Hooks

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,12 +5,16 @@
     <button @click="handleClick">Click me</button>
     <button @click="age++">Increment age</button>
     <input type="text" v-model="name"> <!-- 2 Way Data Binding -->
-
+    
+    <br><br><br>
+    <input type="text" v-model="search">
+    <p>Search term - {{ search }}</p>
+    <div v-for="jedi in matchingJedis" :key="jedi">{{ jedi }}</div>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { computed, reactive, ref, watch, watchEffect } from 'vue'
 
 export default {
   name: 'Home',
@@ -20,15 +24,35 @@ export default {
     // This is done using the template refs to make variables reactive
     const name = ref('Shameel')
     const age = ref(20)
+    //const fullName = reactive('Shameel-Fazul') // reactive is similar to refs but you can't use primitive values. only objects/arrays reactive({ }) reactive([ ])
 
     //const p = ref(null) //Template reference - "null" is for the initial value for the <p> element before returning and getting the DOM value
+    const search = ref('')
+    const jedis = ref(['Anakin', 'Padme', 'Ahsoka', 'Obi Wan', 'Yoda', 'Mace Windu', 'Udamari'])
+ 
+    // Computed Properties - Filtering data based on other data
+    const matchingJedis = computed(() => {
+      return jedis.value.filter((jedi) => jedi.includes(search.value))
+    })
+
+    const stopWatch = watch(search, () => { // Like the useEffect hook in React, runs everytime the search variable changes
+      console.log('watch ran')
+    })
+
+    const stopWatchEffect = watchEffect(() => { // runs on initial load, and then watches and returns updated values
+      console.log('watch effect ran', search.value)
+    })
+
 
    const handleClick = () => {
      name.value = 'codename_shameel' // Updating data using the Composition API
      age.value = 19
+
+     stopWatch() // Invoke to stop watching
+     stopWatchEffect() // Invoke to stop watching
    }
 
-    return { name, age, handleClick} // Value of DOM elements can be accessed after returning
+    return { name, age, handleClick, jedis, search, matchingJedis} // Value of DOM elements can be accessed after returning
   }
 }
 </script>
